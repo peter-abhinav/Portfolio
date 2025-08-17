b0546cfafb3891ac57974c14a3d61baae0273e96
Hello-ship-code <hello.ship.code@gmail.com>
2025-08-17 19:09:27 +0530

    star count fixed

---
+++ src/utils/imagesImport.ts
@@ -0,0 +1,23 @@
+import c from '../assets/icons/c.svg'
+import css3 from '../assets/icons/css3.svg'
+import ejs from '../assets/icons/ejs.svg'
+import grpc from '../assets/icons/grpc.svg'
+import hacker from '../assets/icons/hacker.svg'
+import html5 from '../assets/icons/html5.svg'
+import mongodb from '../assets/icons/mongodb.svg'
+import mysql from '../assets/icons/mysql.svg'
+import railway from '../assets/icons/railway.svg'
+import shell from '../assets/icons/shell.svg'
+
+export const images = {
+  c: { src: c, alt: 'C' },
+  css3: { src: css3, alt: 'CSS3' },
+  ejs: { src: ejs, alt: 'EJS' },
+  grpc: { src: grpc, alt: 'gRPC' },
+  hacker: { src: hacker, alt: 'Hacker' },
+  html5: { src: html5, alt: 'HTML5' },
+  mongodb: { src: mongodb, alt: 'MongoDB' },
+  mysql: { src: mysql, alt: 'MySQL' },
+  railway: { src: railway, alt: 'Railway' },
+  shell: { src: shell, alt: 'Shell' }
+}
